702e00f6d90aa46668fde6637edeee5a8f149a5f
Sachithra Dilshan <itismesachithra@gmail.com>
2019-10-08 23:41:16 +0530

    added animation and modal

--- src/app/mywork/mywork.component.ts
+++ src/app/mywork/mywork.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mywork.component.scss']
 })
 export class MyworkComponent implements OnInit {
-
+  modelimg;
+  modeltitle;
   constructor() { }
 
   ngOnInit() {
   }
-
+  imagetomodel(frist,title){
+  this.modelimg=frist.src;
+  this.modeltitle=title;
+  }
 }
